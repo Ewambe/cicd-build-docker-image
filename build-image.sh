ae7e9ea9666f056ef1d4ec8cdb954ec5ec27ed39
Ewambe <ewambe5@gmail.com>
2023-03-04 00:19:34 -0500

    updated files

---
+++ build-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+#This is the shell script file that codebuild will use to create our docker image
+#when building shell script file, always start with the sheben
+
+# shell script will fail if there is any error
+set -eu
+
+
+# build the docker image
+docker build -f $IMAGE_TAG/Dockerfile -t $IMAGE_TAG .
