d6df980758fa79b6daad2f1bc029d5dae7eaa974
Ewambe <ewambe5@gmail.com>
2023-03-15 23:14:46 -0400

    fixed

--- push-image.sh
+++ push-image.sh
@@ -5,8 +5,12 @@ set -eu
 
 #codebuild will use this file to push image in docker hub
 
+
 # retag docker image 
 docker tag techmax 353182159803.dkr.ecr.us-east-1.amazonaws.com/techmax
+
+# Login to docker
+docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 353182159803.dkr.ecr.us-east-1.amazonaws.com
 
 # login to ecr
 aws ecr get-login-password | docker login --username AWS --password-stdin 353182159803.dkr.ecr.us-east-1.amazonaws.com
