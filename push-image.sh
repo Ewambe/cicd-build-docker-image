98798580a7535343c502c69626e4e8d6b967e4f7
Ewambe <ewambe5@gmail.com>
2023-03-15 23:26:02 -0400

    fixed3

--- push-image.sh
+++ push-image.sh
@@ -9,9 +9,6 @@ set -eu
 # retag docker image 
 docker tag techmax 353182159803.dkr.ecr.us-east-1.amazonaws.com/techmax
 
-# Login to docker
-docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 353182159803.dkr.ecr.us-east-1.amazonaws.com
-
 # login to ecr
 aws ecr get-login-password | docker login --username AWS --password-stdin 353182159803.dkr.ecr.us-east-1.amazonaws.com
 
